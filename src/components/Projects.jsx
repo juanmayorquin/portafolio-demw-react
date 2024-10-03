bb73b47ef0fcb1bc45634e0fb4b908141a5d3f4c
juanmayorquin <juanjmayor@outlook.es>
2024-10-03 15:22:21 -0500

    Se prepara para deploy

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,42 +1,40 @@
 import React from "react";
-import GithubIcon from "../assets/icons/GithubIcon";
-import HeroButton from "./Hero/HeroButton";
 import ProjectsCarousel from "./Projects/ProjectsCarousel";
 
 const Projects = () => {
   const projects = [
     {
-      img: "src/assets/img/login.png",
+      img: "/img/login.png",
       title: "Login DEMW",
       desc: "Proyecto de clase: una página de inicio de sesión creada con HTML y CSS, demostrando habilidades básicas de diseño web.",
       href: "https://github.com/juanmayorquin/login-demw",
     },
     {
-      img: "src/assets/img/portfolio.png",
+      img: "/img/portfolio.png",
       title: "Portafolio DEMW",
       desc: "Proyecto de clase: un portafolio personal creado con HTML y CSS, demostrando habilidades básicas de diseño web y desarrollo frontend.",
       href: "https://github.com/juanmayorquin/portafolio-demw",
     },
     {
-      img: "src/assets/img/react-components.png",
+      img: "/img/react-components.png",
       title: "Ejercicio Componentes React",
       desc: "Actividad en clase: un primer acercamiento a componentes de React para la creación de una página web sencilla.",
       href: "https://github.com/juanmayorquin/ejercicio-componentes-react",
     },
     {
-      img: "src/assets/img/carreer-information.png",
+      img: "/img/carreer-information.png",
       title: "Información sobre ingeniería multimedia",
       desc: "Actividad en clase: un primer acercamiento a HTML y CSS para la creación de una página web sencilla.",
       href: "https://github.com/juanmayorquin/prueba-demw",
     },
     {
-      img: "src/assets/img/comparator.png",
+      img: "/img/comparator.png",
       title: "Comparador",
       desc: "Actividad de clase: una página web que compara dos números, creada con HTML y JavaScript.",
       href: "https://github.com/juanmayorquin/demw-clase-8",
     },
     {
-      img: "src/assets/img/royal-rush.png",
+      img: "/img/royal-rush.png",
       title: "Royal Rush",
       desc: "Proyecto de clase: Videojuego creado con Unity y C# para la asignatura de Computación Gráfica.",
       href: "https://github.com/juanmayorquin/breakfast-rush",
